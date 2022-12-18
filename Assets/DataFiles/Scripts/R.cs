0776d848c6351e01407a1803eef9f53f7c86ed54
ramyacp14 <ramyacp14@gmail.com>
2022-12-18 09:59:35 +0530

    RSRBCard
    RSRBCard

---
+++ Assets/DataFiles/Scripts/R.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class R : MonoBehaviour,IVirtualButtonEventHandler {
+
+    public GameObject bcardgo;
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if(vb.VirtualButtonName== "Button")
+        {
+            Debug.Log("Button is pressed");
+            bcardgo.SetActive(true);
+        }
+        else
+        {
+            throw new UnityException(vb.VirtualButtonName);
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Virtual Button released");
+        bcardgo.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+        VirtualButtonBehaviour vb = GetComponentInChildren<VirtualButtonBehaviour>();
+        vb.RegisterEventHandler(this);
+        bcardgo.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
